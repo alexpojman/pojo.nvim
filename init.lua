27b5c23a18ea206d746267c9a4670c7070b261aa
Alex Pojman <pojman.alex@gmail.com>
2026-03-05 22:47:36 -0600

    Add additional mason auto-install lsps

--- init.lua
+++ init.lua
@@ -680,6 +680,9 @@ require('lazy').setup({
         gopls = {},
         -- pyright = {},
         rust_analyzer = {},
+        vtsls = {},
+        prettier = {},
+        eslint = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
